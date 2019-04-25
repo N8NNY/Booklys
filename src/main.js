b4068a1fefb1fbb74f0706956af95750872e239a
namkhang1875 <ice9966@hotmail.com>
2019-04-26 00:44:43 +0700

    add login system w/ firebase

--- src/main.js
+++ src/main.js
@@ -4,7 +4,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import firebase from 'firebase'
+import {firebaseConfig} from './config'
+
+firebase.initializeApp(firebaseConfig);
 Vue.config.productionTip = false
+
 
 new Vue({
   router,
--- src/store.js
+++ src/store.js
@@ -1,16 +1,45 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import firebase from 'firebase'
+import router from '@/router'
+
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    appName: 'NBK48',
+    user: null,
+    error: null,
+    loading: false
   },
   mutations: {
-
+    setUser (state, payload) {
+      state.user = payload
+    },
+    setError (state, payload) {
+      state.error = payload
+    },
+    setLoading (state, payload) {
+      state.loading = payload
+    }
   },
   actions: {
-
+    //
+    userSignIn({commit}, payload) {
+      commit('setLoading', true)
+      firebase.auth().signInWithEmailAndPassword(payload.email, payload.password)
+        .then(firebaseUser => {
+          commit('setUser', firebaseUser)
+          commit('setLoading', false)
+          commit('setError', null)
+          router.push('/home')
+        })
+        .catch(error => {
+          commit('setError', error.message)
+          commit('setLoading', false)
+        })
+    }
+   //
   }
 })
