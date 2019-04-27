fbb4005d78af90c01db45d4818979d4080c16fd0
L. Chinatip <chinatip1999@hotmail.com>
2019-04-27 19:33:39 +0700

    clear

--- src/config.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyC5qzu9jGoaS0lPEsLN57-5jduifASd_Ks",
-    authDomain: "softdevdb-d19a4.firebaseapp.com",
-    databaseURL: "https://softdevdb-d19a4.firebaseio.com",
-    projectId: "softdevdb-d19a4",
-    storageBucket: "softdevdb-d19a4.appspot.com",
-    messagingSenderId: "300422296810"
-};
